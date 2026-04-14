4bc9af4a9fad5235119c1ae39d98188f3ac5c57e
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-14 01:37:50 -0300

    add Account class with factory method and OnAccountCreatedEvent

--- CleanStore.Domain/AccountContext/Entities/Account.cs
+++ CleanStore.Domain/AccountContext/Entities/Account.cs
@@ -1,10 +1,40 @@
-﻿namespace CleanStore.Domain.AccountContext.Entities;
+﻿using CleanStore.Domain.AccountContext.Events;
+using CleanStore.Domain.AccountContext.ValueObjects;
+using CleanStore.Domain.SharedContext.Entities;
 
-public class Account
+namespace CleanStore.Domain.AccountContext.Entities;
+
+public sealed class Account : Entity, IAggregateRoot
 {
-    public string Emailx { get; set; }
-    
-    public Account()
+    #region Constructors
+
+    private Account() : base(0)
     {
     }
+
+    private Account(
+        int id,
+        Email email) : base(id)
+    {
+
+        Email = email;
+    }
+
+    #endregion
+
+    #region Factories
+
+    public static Account Create(int id, Email email)
+    {
+        var account = new Account(id, email);
+        account.RaiseDomainEvent(new OnAccountCreatedEvent(id, email));
+
+        return account;
+    }
+    
+    #endregion
+    
+    #region Properties
+    public Email Email { get; private set; } = null!;
+    #endregion
 }
---
+++ CleanStore.Domain/AccountContext/Events/OnAccountCreatedEvent.cs
@@ -0,0 +1,5 @@
+﻿using CleanStore.Domain.SharedContext.Events.Abstractions;
+
+namespace CleanStore.Domain.AccountContext.Events;
+
+public sealed record OnAccountCreatedEvent(int Id, string Email) : IDomainEvent;
