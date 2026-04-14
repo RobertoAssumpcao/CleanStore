6630e25a7bc9ac116ce0d0ebcc36bab0c2b1c58e
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-14 00:26:43 -0300

    refactor Email class for improved readability and consistency

--- CleanStore.Domain/AccountContext/ValueObjects/Email.cs
+++ CleanStore.Domain/AccountContext/ValueObjects/Email.cs
@@ -8,15 +8,12 @@ namespace CleanStore.Domain.AccountContext.ValueObjects;
 public sealed partial record Email : ValueObject
 {
     #region Constants
-
     public const int MinLength = 6;
     public const int MaxLength = 160;
     public const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
-
     #endregion
 
     #region Constructors
-
     private Email()
     {
     }
@@ -26,11 +23,9 @@ public sealed partial record Email : ValueObject
         Address = address;
         Hash = hash;
     }
-
     #endregion
 
     #region Factories
-
     public static Email Create(string address)
     {
         if (string.IsNullOrEmpty(address) || string.IsNullOrWhiteSpace(address))
@@ -44,34 +39,23 @@ public sealed partial record Email : ValueObject
 
         return new Email(address, address.ToBase64());
     }
-
     #endregion
 
     #region Properties
-
     public string Address { get; private set; } = string.Empty;
     public string Hash { get; private set; } = string.Empty;
-
     #endregion
     
     #region Operators
-
-    public static implicit operator string(Email email) 
-        => email.ToString();
-
+    public static implicit operator string(Email email) => email.ToString();
     #endregion
 
     #region Overrides
-
-    public override string ToString() 
-        => Address;
-
+    public override string ToString() => Address;
     #endregion
 
     #region Other
-
     [GeneratedRegex(Pattern)]
     private static partial Regex EmailRegex();
-
     #endregion
 }
