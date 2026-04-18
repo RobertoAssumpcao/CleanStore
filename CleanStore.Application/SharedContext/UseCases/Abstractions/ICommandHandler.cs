8a1aafe83d3d735fd1014d037877789ddfccad0a
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-18 00:33:22 -0300

    add generic ICommand and ICommandHandler interfaces for enhanced command handling

--- CleanStore.Application/SharedContext/UseCases/Abstractions/ICommandHandler.cs
+++ CleanStore.Application/SharedContext/UseCases/Abstractions/ICommandHandler.cs
@@ -3,7 +3,13 @@ using MediatR;
 
 namespace CleanStore.Application.SharedContext.UseCases.Abstractions;
 
-public interface ICommandHandler<in TCommand> : IRequestHandler<ICommand, Result>
+public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand, Result>
+    where TCommand : ICommand
 {
-    
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+    where TCommand : ICommand<TResponse>
+    where TResponse : ICommandResponse
+{
 }
