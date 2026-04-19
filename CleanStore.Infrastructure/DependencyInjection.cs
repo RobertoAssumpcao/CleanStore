830b14d8c1fbbdd950ce1781262d6ef76a040a01
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-19 01:14:23 -0300

    add Account mapping and DbContext configuration for account management

---
+++ CleanStore.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using CleanStore.Application.AccountContext.Repositories.Abstractions;
+using CleanStore.Application.SharedContext.Repositories.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanStore.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddTransient<IUnitOfWork, UnitOfWork>();
+        services.AddTransient<IAccountRepository, AccountRepository>();
+
+        return services;
+    }
+}
