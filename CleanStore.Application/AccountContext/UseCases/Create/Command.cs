dda4e73936d0ab9ee212ae75d8ff9f6c968ea443
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-18 23:19:42 -0300

    add Command and Validator classes for account creation use case

--- CleanStore.Application/AccountContext/UseCases/Create/Command.cs
+++ CleanStore.Application/AccountContext/UseCases/Create/Command.cs
@@ -1,6 +1,5 @@
-﻿namespace CleanStore.Application.AccountContext.UseCases.Create;
+﻿using CleanStore.Application.SharedContext.UseCases.Abstractions;
 
-public class Command
-{
-    
-}
+namespace CleanStore.Application.AccountContext.UseCases.Create;
+
+public sealed record Command(string Email) : ICommand<ICommandResponse>;
--- CleanStore.Application/AccountContext/UseCases/Create/Validator.cs
+++ CleanStore.Application/AccountContext/UseCases/Create/Validator.cs
@@ -1,6 +1,15 @@
-﻿namespace CleanStore.Application.AccountContext.UseCases.Create;
+﻿using CleanStore.Domain.AccountContext.ValueObjects;
+using FluentValidation;
 
-public class Validator
+namespace CleanStore.Application.AccountContext.UseCases.Create;
+
+public class Validator : AbstractValidator<Command>
 {
-    
+    public Validator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Email is required");
+    }
 }
