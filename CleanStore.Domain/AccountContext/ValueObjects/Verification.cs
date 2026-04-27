18f38fcf47c869f565f9706f753d20ea01ad28e6
Roberto Assumpção <contatobetoassumpcao@icloud.com>
2026-04-27 01:43:47 -0300

    add Verification value object for account verification process

---
+++ CleanStore.Domain/AccountContext/ValueObjects/Verification.cs
@@ -0,0 +1,9 @@
+﻿using CleanStore.Domain.SharedContext.ValueObjects;
+
+namespace CleanStore.Domain.AccountContext.ValueObjects;
+
+public sealed partial record Verification : ValueObject
+{
+    public string Code { get; } = Guid.NewGuid().ToString("N")[0..6].ToUpper();
+    public DateTime? ExpiresAt { get;  private set; }
+}
